cf0a9ec750ceb65de34ea4156f7d2f7dcaaf6ccb
Andrew & Ken <Andrewken@livegamer.com>
2013-11-04 18:32:56 -0500

    Adding files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   validates(:name, presence: true)
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates_confirmation_of :password, if: lambda { |m| m.password.present? }
+  validates :password, length: { minimum: 6 }  
   before_save { self.email = email.downcase }
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Tracker::Application.routes.draw do
-  get "users/index"
+  resources :users
   get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
