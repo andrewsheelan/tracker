b4a7fa7fe0267ffe12765849df6640562991a98f
Andrew & Ken <Andrewken@livegamer.com>
2013-11-14 18:27:47 -0500

    Pushing changes

---
+++ iphone/SMS/SMSTests/SMSTests.m
@@ -0,0 +1,34 @@
+//
+//  SMSTests.m
+//  SMSTests
+//
+//  Created by Mac Mini on 11/14/13.
+//  Copyright (c) 2013 Tracker. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SMSTests : XCTestCase
+
+@end
+
+@implementation SMSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
